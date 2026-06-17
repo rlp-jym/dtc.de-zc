a7615ab81aff2f1a34778d14b41922b706a6c9b5
J <ralph.jayoma@gmail.com>
2026-06-17 12:26:07 +0000

    recap pipeline lesson

--- pipeline-v1/ingest_data.py
+++ pipeline-v1/ingest_data.py
@@ -2,31 +2,32 @@
 # coding: utf-8
 
 import click
+import psycopg
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
 
 dtype = {
-    "VendorID": "Int64",
+    "VendorID": "Int64", 
     "passenger_count": "Int64",
-    "trip_distance": "float64",
+    "trip_distance": "float64", 
     "RatecodeID": "Int64",
-    "store_and_fwd_flag": "string",
-    "PULocationID": "Int64",
-    "DOLocationID": "Int64",
+    "store_and_fwd_flag": "string", 
+    "PULocationID": "Int64", 
+    "DOLocationID": "Int64", 
     "payment_type": "Int64",
-    "fare_amount": "float64",
-    "extra": "float64",
-    "mta_tax": "float64",
-    "tip_amount": "float64",
+    "fare_amount": "float64", 
+    "extra": "float64", 
+    "mta_tax": "float64", 
+    "tip_amount": "float64", 
     "tolls_amount": "float64",
-    "improvement_surcharge": "float64",
-    "total_amount": "float64",
+    "improvement_surcharge": "float64", 
+    "total_amount": "float64", 
     "congestion_surcharge": "float64"
 }
 
 parse_dates = [
-    "tpep_pickup_datetime",
+    "tpep_pickup_datetime", 
     "tpep_dropoff_datetime"
 ]
 
@@ -40,12 +41,13 @@ parse_dates = [
 @click.option('--month', default=1, type=int, help='Month of the data')
 @click.option('--target-table', default='yellow_taxi_data', help='Target table name')
 @click.option('--chunksize', default=10000, type=int, help='Chunk size for reading CSV')
+
 def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table, chunksize):
     """Ingest NYC taxi data into PostgreSQL database."""
 
-    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
-    url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
-   
+    prefix = r'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
+    url = f"{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz"
+
     engine = create_engine(f'postgresql+psycopg://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
     df_iter = pd.read_csv(
@@ -53,10 +55,11 @@ def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table, ch
         dtype=dtype,
         parse_dates=parse_dates,
         iterator=True,
-        chunksize=chunksize,
+        chunksize=chunksize
     )
 
     first = True
+    
     for df_chunk in tqdm(df_iter):
         if first:
             df_chunk.head(0).to_sql(
@@ -65,13 +68,12 @@ def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table, ch
                 if_exists='replace'
             )
             first = False
+    
         df_chunk.to_sql(
-            name=target_table,
-            con=engine,
+            name=target_table, 
+            con=engine, 
             if_exists='append'
         )
 
-    print("Done.")
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
